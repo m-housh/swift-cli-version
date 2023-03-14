9da9c69442953167f5eb8482663401040e80b715
Michael Housh <mhoush@houshhomeenergy.com>
2023-03-14 18:55:37 -0400

    wip

--- Plugins/GenerateVersionBuildPlugin/GenerateVersionBuildPlugin.swift
+++ Plugins/GenerateVersionBuildPlugin/GenerateVersionBuildPlugin.swift
@@ -8,9 +8,16 @@ struct GenerateVersionBuildPlugin: BuildToolPlugin {
     target: PackagePlugin.Target
   ) async throws -> [PackagePlugin.Command] {
     guard let target = target as? SourceModuleTarget else { return [] }
-    let outputPath = context.pluginWorkDirectory
     let tool = try context.tool(named: "git-version")
+    let outputPath = context.pluginWorkDirectory.appending("Version")
+    
+    try FileManager.default.createDirectory(
+      atPath: outputPath.string,
+      withIntermediateDirectories: true
+    )
+    
     let outputFile = outputPath.appending(subpath: "Version.swift")
+    
     return [
       .buildCommand(
         displayName: "Build With Version",
